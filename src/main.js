ba78143edac9270d9eaacca4e91f8e79b97ef6d0
Jaime delPalacio <jaime.delpalacio@citrix.com>
2016-12-05 13:06:55 -0800

    version for variable height... some problems still on flipped scroll

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ const InfListItem = (props) => {
   return (<div data-index={props.index}>{props.children}</div>);
 }
 
-const initPrefix = (children, minRowHeight) => children.map((_, i) => minRowHeight * (i+1));
 const initSizes = (children, minRowHeight) => children.map(() => minRowHeight);
+const makePrefixArray = (arr, avg) => arr.reduce((prev, el, i) => {
+  const a = (el) => el >= 0 ? el : avg;
+  prev[i] = i === 0 ? a(el) : prev[i-1] + a(el);
+  return prev;
+}, new Array(arr.length));
 
 class InfList extends React.Component {
   constructor(props) {
@@ -14,7 +18,7 @@ class InfList extends React.Component {
     this.state = {
       visibleStart: 0,
       visibleEnd: 0,
-      sizes: initSizes(props.children, props.minRowHeight)
+      sizes: initSizes(props.children, -1)
     };
 
     this.onScroll = this.onScroll.bind(this);
@@ -23,50 +27,112 @@ class InfList extends React.Component {
   componentDidMount() {
     this.measureElements();
     this.computeVisibleItems();
+    this.shouldScroll = true;
   }
 
   componentDidUpdate() {
     this.measureElements();
-    this.scrollIntoView();
+    if (this.props.flip) {
+      this.scrollIntoView();
+      // this.adjustScrollPos();
+    }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.children !== this.props.children) {
       const minRowHeight = nextProps.minRowHeight || this.props.minRowHeight;
+      this.shouldScroll = true;
       this.setState({prefix: initPrefix(nextProps.children, minRowHeight)})
     }
   }
 
   measureElements() {
     const elements = this._div.querySelectorAll('.inner-scroller > *:not(.spacer)');
-    const knownSizes = [].map.call(elements, (el, i) => ({ height: el.clientHeight, index: Number(el.getAttribute('data-index'))}));
+    const knownSizes = [].map.call(elements, (el, i) => {
+      const index = Number(el.getAttribute('data-index'));
+      // don't access the height prop of the element if we don't have to, sice that causes
+      // a re-flow in the rendering engine
+      // const height = this.state.sizes[index] >= 0 ? this.state.sizes[index] : el.getBoundingClientRect().height;
+      const height = el.getBoundingClientRect().height;
+      return {
+        height,
+        index
+      };
+    });
 
     const sizes = knownSizes.reduce((prev, s, i) => {
       prev[s.index] = s.height;
       return prev;
     }, this.state.sizes);
 
-    console.log('s', sizes.join(', '));
-    // console.log('p', prefix.join(', '));
+    this.clientHeight = this._div.clientHeight;
 
+
+    // this.setState({ itemHeightAvg: avg });
+  }
+
+  adjustScrollPos() {
+    if (this.prevScrollHeight !== this._div.scrollHeight) {
+      const delta = this.prevScrollHeight - this._div.scrollHeight;
+      this.prevScrollHeight = this._div.scrollHeight;
+      this.scrollAdjustment = true;
+      this._div.scrollTop -= delta;
+    }
   }
 
   computeVisibleItems() {
-    const scrollHeight = this._div.scrollHeight;
-    const visibleHeight = this._div.clientHeight;
-    const top = this._div.scrollTop;
+    // let count = 0;
+    // const sum = (this.state.sizes.reduce((a, el) => {
+    //   if (el >= 0) {
+    //     count++;
+    //     a += el;
+    //   }
+    //   return a;
+    // }, 0));
 
-    const rowHeight = 25;
-    const totalHeight = this.props.children.length * rowHeight;
-    const visibleRows = visibleHeight / 25;
-    const visibleStart = Math.max(0, Math.floor(top / rowHeight) - 5);
-    const visibleEnd = Math.min(this.props.children.length, visibleStart + visibleRows + 5);
-    const offsetTop = top;
-    const removedFromTop = visibleStart * rowHeight;
-    const removedFromBottom = (this.props.children.length - visibleEnd) * rowHeight
-    const offsetBottom = totalHeight - offsetTop - visibleHeight;
+    // const avg = count > 0 ? sum / count : this.props.minRowHeight;
+
+    // console.log('avg', avg);
 
     // debugger;
+    const prefix = makePrefixArray(this.state.sizes, this.props.minRowHeight);
+    const visibleHeight = this.clientHeight;
+    const top = this.currentScrollTop || this._div.scrollTop;
+    const bottom = top + visibleHeight;
+    const scrollHeight = this._div.scrollHeight;
+
+    let lastRowIndex, lastRowPos;
+    let firstIndex;
+    let visibleRows;
+    if (this.props.flip) {
+      lastRowIndex = prefix.length - 1;
+      if (scrollHeight > visibleHeight) {
+        while (prefix[lastRowIndex] > top + visibleHeight) lastRowIndex--;
+      }
+
+      lastRowPos = prefix[lastRowIndex];
+      firstIndex = lastRowIndex;
+
+      while (lastRowPos - prefix[firstIndex - 1] < visibleHeight) firstIndex--;
+      visibleRows = lastRowIndex - firstIndex + 1;
+    } else {
+      firstIndex = 0;
+      while (prefix[firstIndex] < top) firstIndex++;
+
+      visibleRows = 0;
+      while (prefix[firstIndex + visibleRows] < top + visibleHeight) visibleRows++;
+      lastRowIndex = Math.min(firstIndex + visibleRows, prefix.length-1);
+      lastRowPos = prefix[lastRowIndex];
+    }
+
+    const firstRowPos = firstIndex ? prefix[firstIndex - 1] : 0
+
+    const buffer = 1;
+    const totalHeight = prefix[prefix.length - 1];
+    const visibleStart = Math.max(0, firstIndex - 1);
+    const visibleEnd = Math.min(prefix.length, firstIndex + visibleRows + 1);
+    const removedFromTop = firstRowPos;
+    const removedFromBottom = totalHeight - lastRowPos;
 
     this.setState({
       initialoffset: this._div.scrollTop,
@@ -80,32 +146,27 @@ class InfList extends React.Component {
   }
 
   scrollIntoView() {
-    // const domNode = this._div;
-    // domNode.scrollTop += domNode.scrollHeight;
-
-    // console.log(domNode.scrollTop);
-
+    if (this.shouldScroll) {
+      const domNode = this._div;
+      setTimeout(() => {
+        domNode.scrollTop =  Number.MAX_SAFE_INTEGER;
+      }, 0)
+      this.shouldScroll = false;
+    }
   }
 
-  onScroll() {
-    this.computeVisibleItems();
+  onScroll(e) {
+    this.currentScrollTop = this._div.scrollTop
+    if (!this.scrollAdjustment) {
+      window.requestAnimationFrame(() => this.computeVisibleItems());
+    } else {
+      this.scrollAdjustment = false;
+    }
+    // this.computeVisibleItems();
     // console.log(this._div.scrollTop);
   }
 
   render() {
-    const styles = {
-      outer: {
-        overflow: 'auto',
-        height: 400,
-        border: 'solid 1px blue'
-      },
-
-
-      inner: {
-
-      }
-    };
-
     let visible = [];
 
     for (let j=this.state.visibleStart; j<this.state.visibleEnd; j++) {
@@ -115,6 +176,7 @@ class InfList extends React.Component {
     const debugInfo = (
       <ul>
         {Object.keys(this.state).map((key, i) => <li key={i}>{key}: {this.state[key]}</li>)}
+        <li>Total items: {this.props.children.length}</li>
         <li>Items rendered: {visible.length}</li>
       </ul>
     );
@@ -122,36 +184,52 @@ class InfList extends React.Component {
 
     return (
       <div>
-        <div className="outer-scroller" style={styles.outer} onScroll={this.onScroll} ref={(e) => this._div = e}>
-          <div className="inner-scroller" style={styles.inner}>
+        <div className="outer-scroller" style={this.props.style} onScroll={this.onScroll} ref={(e) => this._div = e}>
+          <div className="inner-scroller">
             <div className="spacer top" style={{height: this.state.offsetTop}}></div>
             {visible}
             <div className="spacer bottom"style={{height: this.state.offsetBottom}}></div>
           </div>
         </div>
         {debugInfo}
+        <button onClick={() => this._div.scrollTop--}>less</button>
+        <button onClick={() => this._div.scrollTop++}>more</button>
       </div>
     );
   }
 }
 
 InfList.propTypes = {
-  minRowHeight: React.PropTypes.number
+  minRowHeight: React.PropTypes.number,
+  flip: React.PropTypes.bool
 };
 
 InfList.defaultProps = {
-  minRowHeight: 25
+  minRowHeight: 60,
+  flip: true
 };
 
 class Main extends React.Component {
   render() {
-    const el = new Array(500).fill(true).map((__, i) => {
-      const style = {height: 25, borderBottom: 'solid 1px red'};
+    const el = new Array(1000).fill(true).map((__, i) => {
+      const h = Math.random() * 100 + 25 + 'px';
+      const style = {height: h, borderBottom: 'solid 1px red'};
       return <div style={style} key={i}>{i}</div>;
     });
 
+    const style = {
+      marginTop: 0,
+      overflow: 'auto',
+      height: 400,
+      border: 'solid 1px blue',
+      transform: 'translateZ(0)'
+    };
+
+      // <InfList>{el}</InfList>
     return (
-      <InfList>{el}</InfList>
+      <InfList style={style}>
+        {el}
+      </InfList>
     );
   }
 }
